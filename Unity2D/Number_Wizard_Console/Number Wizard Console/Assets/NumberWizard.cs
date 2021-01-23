add21e2288c7d3c1ade4c0b6fb63621c97b50227
akak4456 <akak4456@naver.com>
2021-01-23 12:51:12 +0900

    commit

--- Unity2D/Number_Wizard_Console/Number Wizard Console/Assets/NumberWizard.cs
+++ Unity2D/Number_Wizard_Console/Number Wizard Console/Assets/NumberWizard.cs
@@ -7,9 +7,11 @@ public class NumberWizard : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        int max = 1000;
+
         Debug.Log("Welcome to number wizard, yo");
         Debug.Log("Pick a number, don't tell me what it is...");
-        Debug.Log("The highest number you can pick is 1000");
+        Debug.Log("The highest number you can pick is: " + max);
         Debug.Log("The lowest number you can pick is 1");
     }
 
